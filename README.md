395781f0fb925ce3082b225617e1cc1869ab013b
Greg Tronel <greg.tronel.dev@gmail.com>
2018-04-08 19:08:27 -0700

    README

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
-** LeBonCoin Filter/Alert Bot **
+## LeBonCoin Filter/Alert Bot
+----
 
-
-- Find posts related to sales (ventes/offres) on leboncoin.fr and relay them to email address(es)
-- Email and target URL configuration is done in ```config.py```, search parameters are passed as command line arguments (python lbc.py --help)
-- Only tested on real estate sales (ventes immobilieres) but it should be easily extendable to other section from the config file
-    - Feel free to test and confirm
-- Any contribution is welcome
-- Google "cronjob" if interested in running the script on a specific schedule (e.g every day, at the 30th min of each hour)
+### Some notes
+- Find posts related to sales (ventes/offres) on [leboncoin.fr](https://www.leboncoin.fr) and relay them to email address(es)
+- Email and target URL configuration is done in ```config.py```
+- Search parameters are passed as command line arguments (python lbc.py --help)
+- Only tested on real estate sales (ventes immobilieres) but it should be easily extendable to other sections of the site directly from the config file
+    - Feel free to test and confirm to above, thought I'll probably get to it eventually.
+- Google how to setup a "cronjob" if interested in running the script on a specific schedule (e.g every day, at the 30th min of each hour)
 - ~ alpha (docstrings are still missing, but I'll probably get around to it)
+- Any contribution is welcome
 
+## Install/Run on Mac/Linux:
+1. ```$ git clone <this project dir>```
+2. ```$ pip install -r requirements.txt --user```
+3. Edit ```config.py``` with email information
+4. ```$ python lbc.py --help``` for help about search parameters
 
-- Install/Run on Mac/Linux:
-
-```git clone <this project dir>```
-```pip install -r requirements.txt --user```
-```python lbc.py --help```
+### Example Usage:
+To send a mail containing the 10 latest posts about appartments or houses sales in Lyon (districts/arrondissements 1, 2 or 3) between prices 300k and 500k.
+```python lbc.py --region rhone_alpes --location 69001 69002 69003 --minprice 300000 --maxprice 500000 --maxmail 10 --search appartement maison```
 
--- parsecmd.py
+++ parsecmd.py
@@ -6,34 +6,34 @@ def parse_args(print_args=False):
     """
     parser = argparse.ArgumentParser(description="LeBonCoin.fr Filter/Alert Bot")
     
-    parser.add_argument("-r", metavar="region", dest="region",
-                        help="Region", default="ile_de_france")
-    parser.add_argument("-l", metavar="location", dest="location", nargs="+",
-                        help="Location (zip code)", default=["Paris"])
-    parser.add_argument("-s", metavar="search", dest="searches", nargs="+",
+    parser.add_argument("-r", "--region", metavar="region", dest="region",
+                        help="Region", default="")
+    parser.add_argument("-l", "--location", metavar="location", dest="location", nargs="+",
+                        help="Location (zip code)", default=[])
+    parser.add_argument("-s", "--search", metavar="search", dest="searches", nargs="+",
                         help="Searches to perform", default=["Appartement"])
-    parser.add_argument("-minp", metavar="minprice", dest="minprice",
+    parser.add_argument("-minp", "--minprice", metavar="minprice", dest="minprice",
                         help="minimum price value", default="")
-    parser.add_argument("-maxp", metavar="maxprice", dest="maxprice",
+    parser.add_argument("-maxp", "--maxprice", metavar="maxprice", dest="maxprice",
                         help="maximum price value", default="")
-    parser.add_argument("-minr", metavar="minroom", dest="minroom",
+    parser.add_argument("-minr", "--minroom", metavar="minroom", dest="minroom",
                         help="minimum num of rooms", default="")
-    parser.add_argument("-maxr", metavar="maxroom", dest="maxroom",
+    parser.add_argument("-maxr", "--maxroom", metavar="maxroom", dest="maxroom",
                         help="maximum num of rooms", default="")
-    parser.add_argument("-mina", metavar="minarea", dest="minarea",
+    parser.add_argument("-mina", "--minarea", metavar="minarea", dest="minarea",
                         help="minimum area in m2", default="")
-    parser.add_argument("-maxa", metavar="maxarea", dest="maxarea",
+    parser.add_argument("-maxa", "--maxarea", metavar="maxarea", dest="maxarea",
                         help="maximum area in m2", default="")
+    parser.add_argument("-m", "--maxmail", metavar="maxmail", dest="maxmail",
+                        help="Max # of emails sent", default="10")
+    parser.add_argument("-u", "--email_subject", metavar="email-subject", dest="email_subject",
+                        help="Email's subject", default="Item(s) matched your search")
     #parser.add_argument("-e", metavar="email-to", dest="email_to",
     #                    help="Email to send it to", default="")
     #parser.add_argument("-f", metavar="email-from", dest="email_from",
     #                    help="Email to send it from", default="")
-    parser.add_argument("-m", metavar="maxmail", dest="maxmail",
-                        help="Max # of emails sent", default="10")
-    parser.add_argument("-u", metavar="email-subject", dest="email_subject",
-                        help="Email's subject", default="Item(s) matched your search")
-    parser.add_argument("--smtp-server", metavar="smtp-server", dest="server",
-                        help="SMTP server to use", default="localhost")
+    #parser.add_argument("--smtp-server", metavar="smtp-server", dest="server",
+    #                    help="SMTP server to use", default="localhost")
     
     if print_args:
         print '\n'
